0350580e1305c02459423079e3cfebbb8da54b5c
andrey_silivanov <andrey.silivanovv@gmail.com>
2022-03-27 12:15:35 +0300

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div class="container-scroller">
+    <right-block/>
+    <div class="container-fluid page-body-wrapper">
+      <top-block/>
+      <main-block/>
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+</template>
+
+<script>
+import RightBlock from "@/components/RightBlock";
+import TopBlock from "@/components/TopBlock";
+import MainBlock from "@/components/MainBlock";
+
+export default {
+  name: 'App',
+  components: {
+    RightBlock,
+    TopBlock,
+    MainBlock
+  }
+}
+</script>
---
+++ frontend/src/components/MainBlock.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- main-panel start -->
+  <div class="main-panel">
+    <div class="content-wrapper">
+      <div class="row chat">
+        <div class="col-md-12 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded chat-message">
+                <div class="text-md-center text-xl-left">
+                  <h6 class="mb-1">Transfer to Paypal</h6>
+                  <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                </div>
+                <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                  <h6 class="font-weight-bold mb-0">$236</h6>
+                </div>
+              </div>
+              <div class="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded chat-message">
+                <div class="text-md-center text-xl-left">
+                  <h6 class="mb-1">Tranfer to Stripe</h6>
+                  <p class="text-muted mb-0">07 Jan 2019, 09:12AM</p>
+                </div>
+                <div class="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
+                  <h6 class="font-weight-bold mb-0">$593</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- content-wrapper ends -->
+  </div>
+  <!-- main-panel ends -->
+</template>
+
+<script>
+export default {
+  name: "MainBlock"
+}
+</script>
+
+<style scoped>
+
+</style>
